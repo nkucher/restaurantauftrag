421f361c22a26b50e4a726e3e00b341187d3d44f
nkucher <n.kucher@sus.bruelmatt.ch>
2024-03-26 15:46:15 +0100

    add js file

---
+++ main.js
@@ -0,0 +1,9 @@
+const button = document.getElementsByClassName("buttoncl")[0];
+
+console.log(button)
+button.addEventListener("click", buttongetlicked )
+
+
+function buttongetlicked (){
+console.log("hi")
+}
